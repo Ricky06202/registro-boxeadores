831964378b397b3ec672dd964676c5f1e1df3d81
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2025-07-06 16:13:27 -0500

    arreglamos los servicios

--- src/modules/boxeadores/services/boxeadoresService.ts
+++ src/modules/boxeadores/services/boxeadoresService.ts
@@ -1,31 +1,20 @@
-import { parseBoxeadorGC } from "@/modules/shared/logic/boxeadoresConverter";
-import axios from "axios";
+import { parseBoxeadorGC } from '@/modules/shared/logic/boxeadoresConverter'
+import axios from 'axios'
 
 export const getBoxeadores = async () => {
   return axios
-    .get(`${process.env.NEXT_PUBLIC_API_URL}/api/boxeadores`)
-    .then(({ data }) => data.map((box: any) => parseBoxeadorGC(box)));
-};
+    .get(`/api/boxeadores`)
+    .then(({ data }) => data.map((box: any) => parseBoxeadorGC(box)))
+}
 
 export const createBoxeador = async (boxeador: any) => {
-  return axios.post(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/boxeadores`,
-    boxeador
-  );
-};
+  return axios.post(`/api/boxeadores`, boxeador)
+}
 
-export const updateBoxeador = async (
-  id: number,
-  boxeador: any
-) => {
-  return axios.put(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/boxeadores/${id}`,
-    boxeador
-  );
-};
+export const updateBoxeador = async (id: number, boxeador: any) => {
+  return axios.put(`/api/boxeadores/${id}`, boxeador)
+}
 
 export const deleteBoxeador = async (id: number) => {
-  return axios.delete(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/boxeadores/${id}`
-  );
-};
+  return axios.delete(`/api/boxeadores/${id}`)
+}
--- src/modules/boxeadores/services/fieldOptionsService.ts
+++ src/modules/boxeadores/services/fieldOptionsService.ts
@@ -1,79 +1,59 @@
-import { formatearUbicacion } from "@/modules/shared/logic/formatLocation";
-import axios from "axios";
+import { formatearUbicacion } from '@/modules/shared/logic/formatLocation'
+import axios from 'axios'
 
 export const getGeneros = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/generos`
-  );
-  return data.map(({ id, genero }) => ({ label: genero, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/generos`)
+  return data.map(({ id, genero }) => ({ label: genero, id }))
+}
 
 export const getCategorias = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/categorias`
-  );
-  return data.map(({ id, categoria }) => ({ label: categoria, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/categorias`)
+  return data.map(({ id, categoria }) => ({ label: categoria, id }))
+}
 
 export const getNacionalidades = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/nacionalidades`
-  );
+  const { data } = await axios.get<any[]>(`/api/nacionalidades`)
   return data.map(({ id, nacionalidad, countryCode }) => ({
     label: nacionalidad,
     id,
     countryCode,
-  }));
-};
+  }))
+}
 
 export const getLugares = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/lugares`
-  );
+  const { data } = await axios.get<any[]>(`/api/lugares`)
   return data.map(({ id, pais, provincia, ciudad }) => ({
     label: formatearUbicacion({ pais, provincia, ciudad }),
     id,
-  }));
-};
+  }))
+}
 
 export const getPersons = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/persons`
-  );
-  return data.map(({ id, nombre }) => ({ label: nombre, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/persons`)
+  return data.map(({ id, nombre }) => ({ label: nombre, id }))
+}
 
 export const getRoles = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/roles`
-  );
-  return data.map(({ id, role }) => ({ label: role, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/roles`)
+  return data.map(({ id, role }) => ({ label: role, id }))
+}
 
 export const getGuards = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/guards`
-  );
-  return data.map(({ id, guardia }) => ({ label: guardia, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/guards`)
+  return data.map(({ id, guardia }) => ({ label: guardia, id }))
+}
 
 export const getManagers = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/managers`
-  );
-  return data.map(({ id, manager }) => ({ label: manager, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/managers`)
+  return data.map(({ id, manager }) => ({ label: manager, id }))
+}
 
 export const getPromoters = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/promoters`
-  );
-  return data.map(({ id, promotor }) => ({ label: promotor, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/promoters`)
+  return data.map(({ id, promotor }) => ({ label: promotor, id }))
+}
 
 export const getTrainers = async () => {
-  const { data } = await axios.get<any[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/trainers`
-  );
-  return data.map(({ id, trainer }) => ({ label: trainer, id }));
-};
+  const { data } = await axios.get<any[]>(`/api/trainers`)
+  return data.map(({ id, trainer }) => ({ label: trainer, id }))
+}
